f4a9afb033f810d5fdfe100806362e5c088610ba
Zayrin7 <167258887+Zayrin7@users.noreply.github.com>
2024-05-23 19:57:22 -0600

    Create 4mayo.py

---
+++ 4mayo.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx,count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("\U0001f642")
+    
